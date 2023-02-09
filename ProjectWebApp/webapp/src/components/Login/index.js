9b7cdd23d6ec4c4e2d157f01caaf0957196777ce
Stavsavv <zapat1Laa@hotmail.com>
2023-02-09 10:10:49 +0200

    good

--- ProjectWebApp/webapp/src/components/Login/index.js
+++ ProjectWebApp/webapp/src/components/Login/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Redirect } from "react-router-dom"
 import {Container,
-	FormWrap,FormContent,NavLogo1,NavLogo2,Form,FormH1,
-	FormLabel,FormInput,FormImg,FormButton,Text,Navtext,NavtextLink,ErrorM} from './LoginElements'
+  FormWrap,FormContent,NavLogo1,NavLogo2,Form,FormH1,
+  FormLabel,FormInput,FormImg,FormButton,Text,Navtext,NavtextLink,ErrorM} from './LoginElements'
 
 
 
@@ -13,10 +13,12 @@ export default function LoginForm(data) {
   const [status, setstatus] = React.useState("");
   const [errorMessage, setErrorMessage] = React.useState("");
  
+  //check local storage if it has a value
+  const [loggedIn, setLoggedIn] = React.useState(!!localStorage.getItem('emailID'));
 
    const handleSubmit = async (event) => {
-   	   event.preventDefault();
-   	   setErrorMessage("");
+       event.preventDefault();
+       setErrorMessage("");
        const response = await fetch(`http://127.0.0.1:5000/login`,{
         method: 'POST',
         // headers: {
@@ -27,63 +29,62 @@ export default function LoginForm(data) {
       }).then((response) => response.json());
      
       if (response['login_status'] === "success") {
-      	setstatus("succuess");
-      	localStorage.setItem('emailID', email);
+        setstatus("succuess");
+        localStorage.setItem('emailID', email);
+        setLoggedIn(true);
       } else if (response['login_status'] === "fpassword") {
-      	setErrorMessage("Email and password do not match");
-      	localStorage.setItem('emailID', email);
+        setErrorMessage("Email and password do not match");
       } else{
-      	setErrorMessage("Email is not registered");
-      	localStorage.setItem('emailID', email);
+        setErrorMessage("Email is not registered");
       }
 
-     	setstatus(response);
+      setstatus(response);
       setemail("");
       setpassword("");
   } 
 
-  if (status === "succuess") {
-  	//localstorage
-  	return <Redirect to="/products"/>
+  if (loggedIn) {
+    //localstorage
+    return <Redirect to="/products"/>
   } else {
 
 
-	 	return (
-			<>
+    return (
+      <>
 
-				<Container>
-						
-					<FormWrap>	
-						<FormImg img src='./img/CB.png' alt="CB" />	
-						<FormContent>					
-							<Form onSubmit={handleSubmit}>				
-								<ErrorM>{errorMessage}</ErrorM>
-								<FormH1>Log in to your account</FormH1>
-								<FormLabel htmlFor='for'>Email</FormLabel>
-								 <FormInput type='text' value={email} required
-								onChange={e => setemail(e.target.value)}/>
-								 <FormLabel htmlFor='for'>Password</FormLabel>
-								 <FormInput type='password' value={password} required 
-								  onChange={e => setpassword(e.target.value)}/>
-								 <FormButton type='submit'>Sign in</FormButton>
+        <Container>
+            
+          <FormWrap>  
+            <FormImg img src='./img/CB.png' alt="CB" /> 
+            <FormContent>         
+              <Form onSubmit={handleSubmit}>        
+                <ErrorM>{errorMessage}</ErrorM>
+                <FormH1>Log in to your account</FormH1>
+                <FormLabel htmlFor='for'>Email</FormLabel>
+                 <FormInput type='text' value={email} required
+                onChange={e => setemail(e.target.value)}/>
+                 <FormLabel htmlFor='for'>Password</FormLabel>
+                 <FormInput type='password' value={password} required 
+                  onChange={e => setpassword(e.target.value)}/>
+                 <FormButton type='submit'>Sign in</FormButton>
 
-								<Navtext>
-										<NavtextLink to="/Register">Register</NavtextLink>
-								</Navtext>
+                <Navtext>
+                    <NavtextLink to="/Register">Register</NavtextLink>
+                </Navtext>
 
-							</Form>	
-						
-						 
-						</FormContent>
-					
-					</FormWrap>
-						<div>
-							<NavLogo1  to='/'>Your Digital </NavLogo1>
-							<NavLogo2  to='/'> Warehouse</NavLogo2>
-						</div>
-				</Container>
-			</>
-		)
+              </Form> 
+            
+             
+            </FormContent>
+          
+          </FormWrap>
+            <div>
+              <NavLogo1  to='/'>Your Digital </NavLogo1>
+              <NavLogo2  to='/'> Warehouse</NavLogo2>
+            </div>
+        </Container>
+      </>
+    )
   }
 
 }
